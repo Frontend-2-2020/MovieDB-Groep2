f9fe26469ee72aae84f1722dc6fce1dfe57ca7a5
Dzengiz Tafa <dzengiz.tafa@gmail.com>
2019-10-02 09:22:30 +0200

    Base URL's are now read from the config.js file Added some more styling to to adhere to the BEM convention where possible Reduced the movie description to a maximum of 100 characters Added some more comments to better describe the code

--- src/scripts/initOverview.js
+++ src/scripts/initOverview.js
@@ -2,7 +2,7 @@
 //////////
 
 import axios from 'axios';
-import { api_key, baseUrl } from './config';
+import { api_key, baseUrlOverview, baseUrlCardImg, baseUrlDetail } from './config';
 
 
 // Functionality
@@ -14,7 +14,7 @@ export const initOverview = () => {
     clearContent();
 
     // Do the ajax request
-    axios.get(`${baseUrl}&api_key=${api_key}`)
+    axios.get(`${baseUrlOverview}&api_key=${api_key}`)
         .then(res => {
            console.log(res.data);
             // Generate content
@@ -36,22 +36,23 @@ const generateContent = (data) => {
     // Map through each movie in the array & create a Card for each movie
     data.results.map(movie => {
 
-        // Movie props
-        const cardImageUrl = `https://image.tmdb.org/t/p/w300${movie.poster_path}`;
-        const movieDetailLink = `http://localhost:8080/?movie=${movie.id}`;
+        // Generate URI for card image & movie detail by extending the Base-URL's
+        // with props from the current movie
+        const cardImageUrl = `${baseUrlCardImg}${movie.poster_path}`;
+        const movieDetailLink = `${baseUrlDetail}${movie.id}`;
 
         // Create the card & add it to the content div
         const contentDiv = document.getElementById('content');
         contentDiv.innerHTML += `
             <div class="col-md-3 col-sm-6 mb-4">
                 <div class="card h-100 m-auto">
-                    <img src=${cardImageUrl} class="card-img-top" alt="...">
+                    <img src=${cardImageUrl} class="card-img-top" alt=${movie.title}>
                     <div class="card-body d-flex flex-column justify-content-between">
-                        <div class="card-body-section mb-4">
+                        <div class="card-body--section mb-4">
                             <h5 class="card-title">${movie.title}</h5>
-                            <p class="card-text">${movie.overview.substr(0, 200)}...</p>
+                            <p class="card-text">${movie.overview.substr(0, 100)}...</p>
                         </div>
-                        <div class="card-body-section d-flex justify-content-center">
+                        <div class="card-body--section d-flex justify-content-center">
                             <a href=${movieDetailLink} class="btn btn-secondary">Read more</a>
                         </div>
                     </div>
