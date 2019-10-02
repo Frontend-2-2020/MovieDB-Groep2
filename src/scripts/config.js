f3199f0e9386466ab6ade3642db3458caefe385e
Dzengiz Tafa <dzengiz.tafa@gmail.com>
2019-10-02 09:30:54 +0200

    baseUrlDetail now has the correct URL Added .idea folder of Webstorm to the .gitignore

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,4 +1,4 @@
 export const api_key = "cf09aafcb2c7ae356c4089e437aa0ce3";
 export const baseUrlOverview = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc";
 export const baseUrlCardImg = "https://image.tmdb.org/t/p/w300";
-export const baseUrlDetail = "http://localhost:8080/?movie=";
+export const baseUrlDetail = "http://localhost:8080/index.html?movie=";
